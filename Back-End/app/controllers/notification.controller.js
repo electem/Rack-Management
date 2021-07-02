07620793a5ececd6471e1815a10306071d721dbd
vinzy <vinzy@electems.com>
2021-07-02 13:49:31 +0530

    code commit for Sending Email using notificationTable

--- Back-End/app/controllers/notification.controller.js
+++ Back-End/app/controllers/notification.controller.js
@@ -15,7 +15,7 @@ exports.fetchAllNotification = (req, res) => {
     sendNotificationBasedOnStatus(data);
   }).catch(err => {
       res.status(500).send({
-        message: "Error retrieving notifications"
+        message: "Error retrieving notifications"+err
       });
     });
 };
@@ -25,61 +25,36 @@ function sendNotificationBasedOnStatus(data) {
   for (let i = 0; i < data.length; i++) {
 
     let email = data[i].email;
-    let notificationType = data[i].notificationType;
+    let noOfRetry = data[i].noOfRetry;
 
-    if (data[i].notificationType == 'REGISTERED') {
-      query = `UPDATE notifications SET status = 'SENT' WHERE id = ${data[i].id} AND "notificationType" = '${data[i].notificationType}'`;
-      sequelize.query(query).then(data => {
-        if (data[1].rowCount >= 1) {
-          const message = {
-            from: 'developers@electems.com',
-            to: email,
-            subject: 'Registration',
-            text: 'Hello,user has registered, Your new password is:' +
-              'You can login here:' + 'http://localhost:4200/login ' + 'Thank you'
-          };
-          transport.sendMail(message, function (err, info) {
-            if (err) {
-              console.log(err)
-            } else {
-              console.log('mail has sent.');
-              console.log(info);
+    const message = {
+      from: 'developers@electems.com',
+      to: email,
+      subject: 'Registration',
+      text: 'Hello,user has registered, Your new password is:' +
+        'You can login here:' + 'http://localhost:4200/login ' + 'Thank you'
+    };
+      try {
+        transport.sendMail(message, function (err, info) {
+        console.log('mail has sent.');
+        console.log(info);
+        query = `UPDATE notifications SET status = 'SENT' WHERE id = ${data[i].id} AND "noOfRetry"= ${noOfRetry}`;
+        sequelize.query(query).then(data => {
+          if(data[1].rowCount >=1) {
+            console.log(data[1]);
+          }
+    })
+  });
+      } catch (e) {
+          noOfRetry = noOfRetry-1;
+          query = `UPDATE notifications SET status = 'RETRY',"noOfRetry"= ${noOfRetry} WHERE id = ${data[i].id}`;
+          sequelize.query(query).then(data => {
+            if(data[1].rowCount >=1) {
+              console.log(data[1]);
             }
-          });
-          console.log("updated notification WITH STATUS" + notificationType);
+          })
         }
-        else {
-          console.log("Cannot updated notification WITH STATUS" + notificationType);
-        }
-      })
-    }
-    else if (data[i].notificationType == 'CHANGEPASSWORD') {
-      query = `UPDATE notifications SET status = 'SENT' WHERE id = ${data[i].id} AND "notificationType"='${data[i].notificationType}'`;
-      sequelize.query(query).then(data => {
-        if (data[1].rowCount >= 1) {
-          const message = {
-            from: 'developers@electems.com',
-            to: email,
-            subject: 'Change Password',
-            text: 'Hello,Password changed, Your new password is:' +
-              'You can login here:' + 'http://localhost:4200/login ' + 'Thank you'
-          };
-          transport.sendMail(message, function (err, info) {
-            if (err) {
-              console.log(err)
-            } else {
-              console.log('mail has sent.');
-              console.log(info);
-            }
-          });
-          console.log("updated notifications WITH STATUS" + notificationType);
-        }
-        else {
-          console.log("Cannot updated notifications WITH STATUS" + notificationType);
-        }
-      })
-    }
-
+    
   }
 }
 
